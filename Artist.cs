5a833c9f8d8bf6dfd8cae9af6e891360dda2979d
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-05-26 20:09:59 +0200

    created core classes for Song, Artist, and Genres
    - created Song.cs with properties for title, artists, duration, and genre
    - created Artist.cs with methods to manage songs and placeholders for albums
    - created Genres enum with music genres

---
+++ Artist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synth
+{
+    internal class Artist
+    {
+        public string Name { get; set; }
+        private List<Song> Songs { get; set; }
+        // add albums later
+
+        public Artist(string name, List<Song> songs)
+        {
+            this.Name = name;
+            this.Songs = songs;
+        }
+
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+        }
+
+        public void AddAlbum()
+        {
+            // add album class later
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synth
+{
+    internal class Song
+    {
+        public string Title { get; set; }
+        public List<Artist> Artists { get; set; }
+        public Genres SongGenre { get; set; }
+        public int Duration { get; set; }
+
+        public Song(string title, List<Artist> artists, int duration, Genres genre)
+        {
+            this.Title = title;
+            this.Artists = artists;
+            this.Duration = duration;
+            this.SongGenre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {string.Join(", ", Artists)}";
+        }
+    }
+}
