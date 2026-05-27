6d872703e36c7ecca5549611a836545bfc4a67d5
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-05-27 13:22:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Program.cs
+using Synth;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Synth!");
+        // TODO:
+        // Create Client class with LoadSongs method that reads from a file and creates Song and Artist objects
+        // Create ShowAllSongs method that displays all songs in the library as a list
+        // Create a methode for duration of songs in minutes and seconds
+    }
+}
