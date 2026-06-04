764118704810b73dbfd07982e8211ac2d327c5e5
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-06-04 17:49:02 +0200

    Added Windows Media Player playback support via IPlayable
    - Added WMPLib COM reference
    - Created IPlayable interface
    - Updated Song to support playback with WindowsMediaPlayer

--- Song.cs
+++ Song.cs
@@ -1,25 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WMPLib;
 
 namespace Synth
 {
-    internal class Song
+    internal class Song : IPlayable
     {
         public string Title { get; set; }
         public List<Artist> Artists { get; set; }
         public Genres SongGenre { get; set; }
         public int Duration { get; set; }
+        public string FilePath { get; set; }
 
-        
+        // for iplayable
+        public int Length => Duration;
+        // for each song to manage
+        private WindowsMediaPlayer _player;
 
-        public Song(string title, List<Artist> artists, int duration, Genres genre)
+
+        public Song(string title, List<Artist> artists, int duration, Genres genre, string filePath = "")
         {
             this.Title = title;
             this.Artists = artists;
             this.Duration = duration;
             this.SongGenre = genre;
+            this.FilePath = filePath;
+            _player = new WindowsMediaPlayer();
         }
+
+        public void Play()
+        {
+            if(string.IsNullOrEmpty(this.FilePath))
+            {
+                Console.WriteLine($"[Error] Cannot play '{Title}' - file path is missing.");
+                return;
+            }
+            _player.URL = this.FilePath;
+            _player.controls.play();
+            Console.WriteLine($"Playing: {Title} by {string.Join(", ", Artists)}");
+        }
+        public void Pause() { }
+        public void Stop() { }
+        public void Next() { }
 
         public override string ToString()
         {
