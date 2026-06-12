673d154b96e5f6eb0cdd0a76c377f9e3f1585c6f
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-06-12 11:56:38 +0200

    Albums set up

---
+++ Album.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Synth
+{
+    internal class Album
+    {
+        private List<Artist> Artists;
+        public string Title { get; private set; }
+
+        
+        public Album(List<Artist> artist, string title, List<Song> songs) {
+            Artists = artist;
+            Title = title;
+        }
+
+        public List<Artist> ShowArtists()
+        {
+            return Artists;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {string.Join(", ", Artists)}";
+        }
+    }
+}
